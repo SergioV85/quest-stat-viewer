1c9207407039342a12b8ef734348182d8e1f38b1
Sergiy Voznyak <sergiy.voznyak@gmail.com>
2017-08-23 22:16:01 +0200

    update favicon links

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -15,9 +15,9 @@
  */
 module.exports = {
   link: [
-    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/icon/favicon-32x32.png' },
-    { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/assets/icon/favicon-96x96.png' },
-    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/icon/favicon-16x16.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/icon/favicon-32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/assets/icon/favicon-96.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/icon/favicon-16.png' },
 
     /**
      * <link> tags for a Web App Manifest
